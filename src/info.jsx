bad2c76ee69957111b13256e980302bf731ee424
Hamdi Chaieb <chaiebhamdi13@gmail.com>
2025-11-20 13:40:30 +0100

    First commit, working app

--- src/info.jsx
+++
@@ -1,6 +0,0 @@
-export const  info =[
-    {id:0, nom: "Hamdi Chaieb", qualif: "3LIG G2", imageSrc: "hamdi.jpg"},
-    {id:1, nom: "Alaeddine Charrada", qualif: "3LIG G2", imageSrc: "alaeddine.jpg"},
-    {id:2, nom: "Harry Maguire", qualif: "3LIG G2",  imageSrc: "harry.jpg"},
-];
-export default info;
